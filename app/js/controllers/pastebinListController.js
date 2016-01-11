e6c1342e4574ae555defda604ee05762ea2611cd
tomek <tomankieta@gmail.com>
2016-01-11 23:47:28 +0100

    add date filtering, add improve fulltext filtering

--- app/js/controllers/pastebinListController.js
+++ app/js/controllers/pastebinListController.js
@@ -1,50 +1,84 @@
 (function(angular) {
 	"use strict";
 
-	angular.module('app').controller('PastebinListController',
-			[ 'pastebinService', 'userService', '$state', '$scope', '$timeout', function(pastebinService, userService, $state, $scope, $timeout) {
-				var that = this;
+	angular.module('app').controller(
+			'PastebinListController',
+			[
+					'pastebinService',
+					'userService',
+					'$state',
+					'$scope',
+					'$timeout',
+					function(pastebinService, userService, $state, $scope, $timeout) {
+						var that = this;
+						var dateToStringFormat = function(date) {
+							return ("0" + date.getDate()).slice(-2) + ("0" + (date.getMonth() + 1)).slice(-2) + date.getFullYear();
+						};
 
-				pastebinService.getPastebins().then(function(results) {
-					that.pastebins = results;
-					that.displayPastebins = [].concat(that.pastebins);
-				});
+						var milisecondsToStringFormat = function(date) {
+							var filterDate = null;
 
-				that.isErasable = function(pastebin) {
-					return pastebin.user_id === userService.getUserId();
-				};
+							filterDate = new Date(date.substring(4, 9), (date.substring(2, 4) - 1), date.substring(0, 2));
+							return dateToStringFormat(filterDate);
+						};
 
-				that.isEditable = function(pastebin) {
-					return pastebin.user_id === userService.getUserId();
-				};
-
-				that.removePastebin = function(id, position) {
-					var result = pastebinService.deletePastebin(id);
-					result.then(function() {
-						that.displayPastebins.splice(position, 1);
-					});
-				};
-
-				that.edit = function(pastebin) {
-					$state.go("editPastebin", {
-						id : pastebin.id
-					});
-				};
-				
-				var dateFilterWatch = $scope.$watch('ctrl.dateFilter', function(newValue){
-					var filterDate = null;
-					var parsedDateString = null;
-					
-					if(newValue && newValue.length === 8){
-						filterDate = new Date(newValue.substring(4, 9), (newValue.substring(2, 4)-1), newValue.substring(0, 2));
-						parsedDateString = ("0" + filterDate.getDate()).slice(-2) + ("0" + (filterDate.getMonth()+1)).slice(-2) + filterDate.getFullYear();
-						if(parsedDateString !== newValue){
-							$timeout(function(){
-								that.dateFilter = parsedDateString; 
+						pastebinService.getPastebins().then(function(results) {
+							that.pastebins = results;
+							that.displayPastebins = [];
+							that.pastebins.forEach(function(element) {
+								that.displayPastebins.push({
+									dateInString : dateToStringFormat(new Date(element.date)),
+									pastebin : element
+								});
 							});
-						}
-					}
-				});
-				
-			} ]);
+						});
+
+						that.isErasable = function(pastebin) {
+							return pastebin.user_id === userService.getUserId();
+						};
+
+						that.isEditable = function(pastebin) {
+							return pastebin.user_id === userService.getUserId();
+						};
+
+						that.removePastebin = function(id, position) {
+							var result = pastebinService.deletePastebin(id);
+							result.then(function() {
+								that.displayPastebins.splice(position, 1);
+							});
+						};
+
+						that.edit = function(pastebin) {
+							$state.go("editPastebin", {
+								id : pastebin.id
+							});
+						};
+
+						var dateFilterWatch = $scope.$watch('ctrl.dateFilter', function(newValue) {
+							var filterDate = null;
+							var parsedDateString = null;
+
+							if (newValue && newValue.length === 8) {
+								filterDate = new Date(newValue.substring(4, 9), (newValue.substring(2, 4) - 1), newValue.substring(0, 2));
+								parsedDateString = ("0" + filterDate.getDate()).slice(-2) + ("0" + (filterDate.getMonth() + 1)).slice(-2) + filterDate.getFullYear();
+								if (parsedDateString !== newValue) {
+									$timeout(function() {
+										that.dateFilter = parsedDateString;
+									});
+								}
+							}
+						});
+
+						that.fulltextFilter = function(item) {
+							if (!that.textFilter) {
+								return true;
+							} else if (String(item.pastebin.username).indexOf(that.textFilter) !== -1 || String(item.pastebin.language).indexOf(that.textFilter) !== -1
+									|| String(item.pastebin.code).indexOf(that.textFilter) !== -1) {
+								console.log(that.textFilter);
+								return true;
+							}
+							return false;
+						};
+
+					} ]);
 })(angular);
