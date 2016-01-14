1e2f0756d0fb32e898d3d76efd3129d249dc8f0e
tomek <tomankieta@gmail.com>
2016-01-14 11:29:35 +0100

    add login/registration page

---
+++ app/js/controllers/userController.js
@@ -0,0 +1,19 @@
+(function(angular) {
+	"use strict";
+
+	angular.module('app').controller('UserController', [ function() {
+		var that = this;
+		
+		that.signup = function(){
+			
+		};
+		
+		that.login = function(){
+			
+		};
+		
+		that.logout = function(){
+			
+		};
+	} ]);
+})(angular);
